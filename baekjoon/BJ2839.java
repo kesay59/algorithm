60b5e2f3588bc5d6f109be227740da1faf53bfb9
kesay59 <kesay59@gmail.com>
2020-10-06 23:51:22 +0900

    BJ 1839

---
+++ baekjoon/BJ2839.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+//BJ 2839 설탕 배달
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int weight = sc.nextInt();
+		int cnt = weight / 5;
+		weight-=cnt*5;
+		for(int i = 0; i <= cnt; i++) {
+			if(weight % 3 == 0) {
+				System.out.println(cnt - i + weight/3);
+				System.exit(0);
+			}
+			weight += 5;
+		}
+		System.out.println(-1);
+	}
+
+}
