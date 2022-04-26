f0e7f48f20668863a6373d09301d278c1cea2e98
Keonsang Yun <kesay59@gmail.com>
2022-04-26 22:52:59 +0900

    BJ : 안테나(18310)

---
+++ baekjoon/BJ18310.js
@@ -0,0 +1,25 @@
+function solution(input) {
+    const num = Number(input[0]);
+    const house = input[1].split(' ');
+    for(let i = 0; i < num; i++) {
+        house[i] = Number(house[i]);
+    }
+    house.sort((a, b) => {
+        return a-b;
+    });
+    console.log(house[Math.floor((num-1)/2)]);
+}
+
+var readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+const input = [];
+rl.on("line", function(line){
+    input.push(line);
+    // rl.close();
+}).on("close", function(){
+    solution(input);
+    process.exit();
+})
