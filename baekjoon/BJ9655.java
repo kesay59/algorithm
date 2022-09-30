d0e87d51198fcc67f478c5a93c55f5b8f83bef00
Keonsang Yun <kesay59@gmail.com>
2022-09-30 20:30:49 +0900

    BJ : 돌 게임(9655)

--- baekjoon/BJ9655.java
+++ baekjoon/BJ9655.java
@@ -6,6 +6,6 @@ public class BJ9655 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        System.out.println(N < 3 ? "SK" : (N % 2 == 1 ? "SK" : "CY"));
+        System.out.println(N % 2 == 1 ? "SK" : "CY");
     }
 }
